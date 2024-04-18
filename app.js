c196aeffb96efda00ca03d1575096108adcab423
mmors <m.morski@gmail.com>
2024-04-18 06:13:03 +0200

    Basics and core concepts

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
-Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
-      goals: [],
-      enteredValue: "",
+      message: "Finish the course and learn Vue!",
     };
   },
-  methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = "";
-    },
-  },
-}).mount("#app");
+});
 
-/* const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
-
-function addGoal() {
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
-    listEl.appendChild(listItemEl);
-    inputEl.value = '';
-}
-
-buttonEl.addEventListener('click', addGoal);
-  */
+app.mount("#user-goal");
