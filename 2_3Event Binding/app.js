a9262c90d572593f2c3ae396aa5cd4a25bd4a3d5
mmors <m.morski@gmail.com>
2024-04-19 06:47:29 +0200

    Pass values to methods from input element. Use $event parameter

--- 2_3Event Binding/app.js
+++ 2_3Event Binding/app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
@@ -10,6 +11,9 @@ const app = Vue.createApp({
     },
     reduceCounter(num) {
       this.counter = this.counter - num;
+    },
+    updateName(event, secondNeme) {
+      this.name = event.target.value + " " + secondNeme;
     },
   },
 });
